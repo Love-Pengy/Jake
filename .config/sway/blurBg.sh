0825b87d4d846918c8c8063c65ad34c2fcceea76
Love-Pengy <afrazierboy2001@gmail.com>
2025-03-19 16:11:11 -0400

    make blur script a bit faster

--- .config/sway/blurBg.sh
+++ .config/sway/blurBg.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-grimshot save output /tmp/blurDesktop.png > /dev/null
-convert -scale 10% -blur 0x.75 -resize 1000% /tmp/blurDesktop.png /tmp/blurDesktop.png  
-convert /tmp/blurDesktop.png -gravity center ~/.config/sway/jakeSmol.png -composite /tmp/blurDesktop.png
+# grimshot save output /tmp/blurDesktop.png > /dev/null
+grim /tmp/lockscreen.png &&\
+convert -filter Gaussian -resize 25% -blur 0x1 -resize 400% /tmp/lockscreen.png /tmp/lockscreen.png
+convert /tmp/lockscreen.png -gravity center ~/.config/sway/jakeSmol.png -composite /tmp/lockscreen.png
+# convert -scale 25% -resize 400% -blur 0x1 /tmp/lockscreen.png /tmp/lockscreen.png  
+# convert -blur 0x4 /tmp/lockscreen.png /tmp/lockscreen.png  
 swaylock --inside-color 00000000\
          --indicator-thickness 15\
          --indicator-radius 125\
          --key-hl-color 8fccf9\
          --ring-color fbf5b9\
-         -f -e -i /tmp/blurDesktop.png
+         -f -e -i /tmp/lockscreen.png
+
+
